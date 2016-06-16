4ee1522f8dd469434f91b8ce26b71e479a227875
JorgePuente <jorge.alejandro.puente@gmail.com>
2016-06-16 13:05:10 -0500

    faltantes

---
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/JoGe/Documents/IONIC/lavadoApp/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/Users/JoGe/Documents/IONIC/lavadoApp/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/phonegap-facebook-plugin/lavadoapp537009-FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/JoGe/Documents/IONIC/lavadoApp/platforms/android/phonegap-facebook-plugin/lavadoapp537009-FacebookLib/bin/AndroidManifest.xml : \
+/Users/JoGe/Documents/IONIC/lavadoApp/platforms/android/phonegap-facebook-plugin/lavadoapp537009-FacebookLib/AndroidManifest.xml \
